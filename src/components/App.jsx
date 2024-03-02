637bef02a73049ed17dec572cc71625c15a6a430
Ondrii1 <135643410+Ondrii1@users.noreply.github.com>
2024-03-02 19:20:07 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,9 @@ import { Profile } from "./Profile/Profile";
 import userData from '../userData.json';
 import { FriendList } from "./FriendList/FriendList";
 import friends from '../friends.json'
-import transactions from '../transactions.json'
 import { TransactionHistory } from "./TransactionHistory/TransactionHistory";
+import transactions from '../transactions.json'
+
 
 export const App = () => {
   return (
