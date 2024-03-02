46a77ce33db01d98ba665c7eb9759002026407f2
Ondrii1 <135643410+Ondrii1@users.noreply.github.com>
2024-03-02 17:59:52 +0200

    update4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,22 @@
 
-function App() {
-  
+import { Profile } from './Profile/Profile';
+import userData from "../userData.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
 
-  return <div>App</div>;
-    
-}
 
-export default App;
+const App = () => {
+ return (
+ <>
+ <Profile
+name={userData.username}
+tag={userData.tag}
+ location={userData.location}
+image={userData.avatar}
+ stats={userData.stats}
+ />
+ <FriendList friends={friends} />
+ <TransactionHistory items={transactions} />
+ </>
+ );
+};
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,26 +1,27 @@
-<div>
-  <div>
-    <img
-      src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-      alt="User avatar"
-    />
-    <p>Petra Marica</p>
-    <p>@pmarica</p>
-    <p>Salvador, Brasil</p>
-  </div>
-
-  <ul>
-    <li>
-      <span>Followers</span>
-      <span>1000</span>
-    </li>
-    <li>
-      <span>Views</span>
-      <span>2000</span>
-    </li>
-    <li>
-      <span>Likes</span>
-      <span>3000</span>
-    </li>
-  </ul>
-</div>
+import css from './Profile.module.css';
+export const Profile = ({ name, tag, location, image, stats }) => {
+    return (
+<div className={css.profile}>
+      <div className={css.description}>
+        <img src={image} alt={name} className={css.avatar} />
+        <p className={css.name}>{name}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
+      </div>
+      <ul className={css.stats}>
+        <li className={css.li}>
+          <span className={css.label}>Followers</span>
+          <span className={css.value}>{stats.followers}</span>
+        </li>
+        <li className={css.liItem}>
+          <span className={css.label}>Views</span>
+          <span className={css.value}>{stats.views}</span>
+        </li>
+        <li className={css.li}>
+          <span className={css.label}>Likes</span>
+          <span className={css.value}>{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
